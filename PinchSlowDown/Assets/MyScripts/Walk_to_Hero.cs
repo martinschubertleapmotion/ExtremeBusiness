1d6ac378ea3fe309eac1723c38f003e7cea0d862
Martin Schubert <mschubert@leapmotion.com>
2016-04-24 01:17:11 -0700

    Added project.

---
+++ PinchSlowDown/Assets/MyScripts/Walk_to_Hero.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walk_to_Hero : MonoBehaviour {
+	public NavMeshAgent agent;
+	public Vector3 destination;
+	// Use this for initialization
+	void Start () 
+	{
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		//////////////////////////////////////////////////////////////////////////////////////////
+		//You don't have to include this one line code:
+		//I included this line to correct my enemy's rotation, you might not need this one line.
+		//Or you may have to change the rotation to fit your character.
+		//////////////////////////////////////////////////////////////////////////////////////////
+		agent.destination = destination;
+//		Internal_Nav_Mesh_Agent.SetDestination (Object_to_Walk_to.transform.position);
+	}
+}
+
