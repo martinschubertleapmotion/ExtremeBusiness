aa4fee4a361d3e168581d2e7a0efaa25b8c1be25
Martin Schubert <mschubert@leapmotion.com>
2016-04-24 23:49:47 -0700

    Briefcase able to be picked up but locked to scene object, can't do prefab yet.

---
+++ PinchSlowDown/Assets/Plugins/RootMotion/PuppetMaster/_DEMOS/Assets/Scripts/PropDrop.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using RootMotion.Dynamics;
+
+namespace RootMotion.Demos {
+
+	// Code example for picking up/dropping props.
+	public class PropDrop : MonoBehaviour {
+
+		public bool IsHolding;
+
+		[Tooltip("The Prop you wish to pick up.")] 
+		public Prop prop;
+
+		[Tooltip("The PropRoot to connect it to.")] 
+		public PropRoot connectTo;
+		
+		void Update () {
+			
+			if (IsHolding = false) {
+				// By setting the prop root's currentProp to null, the prop connected to it will be dropped.
+				connectTo.currentProp = null;
+			}
+		}
+	}
+}
