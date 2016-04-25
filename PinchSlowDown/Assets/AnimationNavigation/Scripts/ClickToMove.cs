4217f6cf1b79311a1aa3a0b8d42669911f70bde5
Martin Schubert <mschubert@leapmotion.com>
2016-04-24 19:37:10 -0700

    Fixed puppetmaster springyness.

---
+++ PinchSlowDown/Assets/AnimationNavigation/Scripts/ClickToMove.cs
@@ -0,0 +1,17 @@
+﻿// ClickToMove.cs
+using UnityEngine;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class ClickToMove : MonoBehaviour {
+
+	public Vector3 Target;
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	void Update () {
+
+				agent.destination = Target;
+		}
+	}
